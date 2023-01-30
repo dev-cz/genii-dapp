94f504919729e1e78c6264d93c782f2343bf7447
Z-Dev <51761060+dev-cz@users.noreply.github.com>
2023-01-29 19:13:15 -0700

    added disclaimer

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -358,10 +358,11 @@ function Main() {
 function Footer() {
   return (
     <footer className={styles.footer}>
+      <div className="flex items-center h-full text-center font-bold text-xl text-transparent bg-clip-text bg-gradient-to-r from-[#ff44c9] to-[#00b8fa]">
+        Created by: ꧁CryptoZii꧂
+      </div>
+      <div className="flex items-center text-white">Note: This dApp is community managed and not officially supported by the nahmii team.</div>
       <div></div>
-      <div className="flex items-center"></div>
-
-      <div className="flex items-center"></div>
     </footer>
   )
 }
